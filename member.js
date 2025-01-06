be59ea7dc2ca1d7d644dfea282f9ba7dd6274658
zhang.william <39043782+moulongzhang@users.noreply.github.com>
2025-01-06 09:14:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return (
+    <div className="skills__member">
+      <div className="skills__member__photo"></div>
+      <div className="skills__member__name">Alex</div>
+      <div className="skills__member__skill">JavaScript</div>
+    </div>
+  );
+}
